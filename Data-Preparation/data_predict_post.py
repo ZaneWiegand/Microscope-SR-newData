0b72ce982e2f29f992110237a2246f1b1897096b
Cocytus-Leon <smilingbaboon@126.com>
2022-05-14 11:36:12 +0800

    update

--- Data-Preparation/data_predict_post.py
+++ Data-Preparation/data_predict_post.py
@@ -1,5 +1,6 @@
 # %%
 import tifffile as tf
+import cv2 as cv
 print("OK!")
 # %%
 hr_origin_dir = '../Data-Post-upsample/20x_origin'
@@ -31,3 +32,22 @@ for pic_num in pic_list:
             lr = img10x[j:j + lr_size, i:i + lr_size]
             tf.imwrite(f'{lr_save_dir}/10x{lr_data_num}.tif', lr)
             lr_data_num += 1
+# %%
+for pic_num in pic_list:
+    for flip_f in [-1, 0, 1]:
+        img20x = tf.imread(f'{hr_origin_dir}/20x{pic_num}.tif')
+        img10x = tf.imread(f'{lr_origin_dir}/10x{pic_num}.tif')
+        img20x = img20x[60:-60, 80:-80]
+        img10x = img10x[30:-30, 40:-40]
+        img20x = cv.flip(img20x, flip_f)
+        img10x = cv.flip(img10x, flip_f)
+        for i in range(0, img20x.shape[1] - hr_size + 1, hr_stride_x):
+            for j in range(0, img20x.shape[0] - hr_size + 1, hr_stride_y):
+                hr = img20x[j:j + hr_size, i:i + hr_size]
+                tf.imwrite(f'{hr_save_dir}/20x{hr_data_num}.tif', hr)
+                hr_data_num += 1
+        for i in range(0, img10x.shape[1] - lr_size + 1, lr_stride_x):
+            for j in range(0, img10x.shape[0] - lr_size + 1, lr_stride_y):
+                lr = img10x[j:j + lr_size, i:i + lr_size]
+                tf.imwrite(f'{lr_save_dir}/10x{lr_data_num}.tif', lr)
+                lr_data_num += 1
