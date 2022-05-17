8498463c1a4109f82cf3117db530d496346ad4e8
Cocytus-Leon <smilingbaboon@126.com>
2022-05-17 14:43:37 +0800

    update

--- nn-EDSR/test.py
+++ nn-EDSR/test.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     weights_file = './weight_output/epoch_67.pth'
     upscale_factor = 2
     plus = 1
-    number = 96
+    number = 80
     print('real data:')
     results = {'psnr': [], 'ssim': [], 'nqm': []}
     for pic_number in range(number):
--- nn-SRGAN/test.py
+++ nn-SRGAN/test.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     weights_file = './weight_output/netG_F2_epoch_38.pth'
     upscale_factor = 2
     plus = 1
-    number = 96
+    number = 80
     print('real data:')
     results = {'psnr': [], 'ssim': [], 'nqm': []}
     for pic_number in range(number):
